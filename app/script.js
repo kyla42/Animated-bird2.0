680cd78be82cf337e4a4dc299e0781a935a4f027
kyla42 <103237579+kyla42@users.noreply.github.com>
2022-06-28 15:35:55 +0900

    add keydown

--- app/script.js
+++ app/script.js
@@ -1,11 +1,19 @@
 const birdBtn = document.querySelector(".bird");
 const movement = ["shake", "jump", "stretch1", "stretch2", "flower", "glasses", "forward", "sliding", "incline"];
 
-birdBtn.addEventListener("click", function () {
+const action = function() {
     for (let i = 0; i < movement.length; i++) {
         birdBtn.classList.remove(movement[i]);
     }
 
     const randomMove = movement[Math.floor(Math.random() * movement.length)];
     birdBtn.classList.add(randomMove);
-});
+}
+
+birdBtn.addEventListener("click", action)
+document.addEventListener("keydown", function(e) {
+    console.log(e.key)
+    if (e.key === "Enter") {
+        action();
+    }
+})
